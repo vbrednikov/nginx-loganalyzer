a9b888f6af4fa5f394c9bb5b474f5dda5b68865b
Vladimir Brednikov <vbrednikov@gmail.com>
2019-01-11 01:31:41 +0300

    bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='nginx-loganalyzer',
-    version='0.1.28',
+    version='0.1.29',
     license='BSD 2-Clause License',
     description='access log statistics generator',
     long_description='%s\n%s' % (
