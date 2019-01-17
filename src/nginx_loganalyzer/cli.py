342f425a6c447f7bff45df973927103e0451eb20
Vladimir Brednikov <vbrednikov@gmail.com>
2019-01-18 01:02:59 +0300

    add config parsing

--- src/nginx_loganalyzer/cli.py
+++ src/nginx_loganalyzer/cli.py
@@ -17,6 +17,7 @@ Why does this file exist, and why not put this in __main__?
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
 import argparse
+import ConfigParser
 import os
 # import decimal
 # import json
@@ -25,13 +26,11 @@ from pprint import pprint
 from log_parse import LogReqtimeStat
 
 from nginx_loganalyzer import get_latest_filename
-from nginx_loganalyzer.config import Config
 
 config = {
-    "REPORT_SIZE": 1000,
+    "REPORT_SIZE": '1000',
     "REPORT_DIR": "./reports",
     "LOG_DIR": "./log",
-
 }
 
 
@@ -45,24 +44,47 @@ def threshold_t(string):
     return value
 
 
-def parse_args(args):
+def parse_args(args, default_config=None):
     parser = argparse.ArgumentParser(description='Find nginx log file,'
                                      ' parse it and produce the html report.')
-    parser.add_argument('--config', default=None, type=argparse.FileType('r'),
+    parser.add_argument('--config', default=default_config,
+                        type=argparse.FileType('r'),
                         help="Path to the config file")
     parser.add_argument('--err-threshold', dest='threshold', default=66,
                         type=threshold_t, help="Parsing error threshold")
-
     return parser.parse_args(args)
 
 
+class ConfigDict(dict):
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+
 def main(args=None):
-    args = parse_args(args=args)
-    the_conf = Config(config, args.config)
+    args = parse_args(args=args, default_config='~/.analyzer.cfg')
+    c = ConfigParser.ConfigParser(config)
+    section = 'DEFAULT'
+    if args.config:
+        c.readfp(args.config)
+        section = c.sections()[0]
+    the_conf = ConfigDict(c.items(section))
 
     logfile_pattern = r'nginx-access-ui.log-' + \
                       r'(?P<yyyy>\d\d\d\d)(?P<mm>\d\d)(?P<dd>\d\d)' + \
                       r'(?P<ext>\.gz)?'
+
     files = (f for f in os.listdir(the_conf.log_dir))
     latest_tuple = get_latest_filename(logfile_pattern, files, the_conf.log_dir)
     log_parser = LogReqtimeStat(latest_tuple, the_conf)
