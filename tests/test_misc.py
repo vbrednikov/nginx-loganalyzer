2e2e7f8ca6e928a098f3dea4a4ce7e0e759dde96
Vladimir Brednikov <vbrednikov@gmail.com>
2019-01-21 01:39:15 +0300

    add tests for ConfigDict structure

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -6,6 +6,7 @@ import shutil
 import tempfile
 import unittest
 
+from nginx_loganalyzer import ConfigDict
 from nginx_loganalyzer import LogFileTuple
 from nginx_loganalyzer import get_latest_filename
 from nginx_loganalyzer import setup_reports
@@ -72,6 +73,52 @@ class TestSetupReports(unittest.TestCase):
         rep_dir_name = os.path.join(self.tmp_dir, 'reports')
         self.assertTrue(setup_reports(rep_dir_name))
         self.assertTrue(os.path.exists(os.path.join(rep_dir_name, 'jquery.tablesorter.min.js')))
+
+
+class TestLogFileTuple(unittest.TestCase):
+    def test_logfile_tuple(self):
+        lft = LogFileTuple('name', 'gz', datetime.date(2017, 12, 31))
+        self.assertEqual(lft.filename, 'name')
+        self.assertEqual(lft.type, 'gz')
+        self.assertEqual(lft.date.strftime('%m/%d/%Y'), '12/31/2017')
+
+
+class TestConfigDict(unittest.TestCase):
+    def setUp(self):
+        self.config = ConfigDict({'a': 2, 'b': 3, 'd': 4, 'e': 9})
+
+    def tearDown(self):
+        del(self.config)
+
+    def test_access_attr(self):
+        self.assertEqual(self.config.a, 2)
+
+    def test_access_non_existing_attr(self):
+        with self.assertRaises(AttributeError):
+            self.config.x
+
+    def test_access_key(self):
+        self.assertEqual(self.config['a'], 2)
+
+    def test_update_attr(self):
+        self.config.b = 5
+        self.assertEqual(self.config.b, 5)
+
+    def test_update_key(self):
+        self.config['d'] = 8
+        self.assertEqual(self.config.d, 8)
+
+    def test_delete_key(self):
+        del(self.config['e'])
+        self.assertFalse(hasattr(self.config, 'e'))
+
+    def test_delete_attr(self):
+        del(self.config.e)
+        self.assertFalse(hasattr(self.config, 'e'))
+
+    def test_delete_non_existing_attr(self):
+        with self.assertRaises(AttributeError):
+            del(self.config.y)
 
 
 if __name__ == '__main__':
